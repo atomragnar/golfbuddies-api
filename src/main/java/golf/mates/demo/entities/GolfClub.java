5e5eac90ba3bfacdbc3686226c4c078d25c2bced
atomragnar <johanneshornbrinck@gmail.com>
2023-03-29 11:47:37 +0200

    Initial commit

---
+++ src/main/java/golf/mates/demo/entities/GolfClub.java
@@ -0,0 +1,38 @@
+package golf.mates.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GolfClub {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String club;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "location_id", nullable = false)
+    private Location location;
+
+    public GolfClub(String club, Location location) {
+        this.club = club;
+        this.location = location;
+    }
+
+    public void setLocation(Location location) {
+
+    }
+
+
+
+}
